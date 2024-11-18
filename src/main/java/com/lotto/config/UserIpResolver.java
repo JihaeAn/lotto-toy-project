33d1e13d80525af5093ee306f93a373eaa9bdb08
jihae <dkswlgo6615@naver.com>
2024-11-18 20:42:04 +0900

    [FEAT] 유저 IP 설정하는 ArgumentResolver 수정, 가장 많이 나온 번호 통계 정렬 추가

--- src/main/java/com/lotto/config/UserIpResolver.java
+++ src/main/java/com/lotto/config/UserIpResolver.java
@@ -4,11 +4,13 @@ import com.lotto.userLotto.dto.UserLotto;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdviceAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Type;
+import java.util.List;
 
 
 // WebConfig 파일에 따로 등록하지 않아도 됨
@@ -30,10 +32,28 @@ public class UserIpResolver extends RequestBodyAdviceAdapter {
     @Override
     public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
 
-        if (body instanceof UserLotto) {
-            UserLotto userLotto = (UserLotto) body;
+        String clientIp = null;
+        UserLotto userLotto = (UserLotto) body;
+
+        List<String> headerList = List.of(
+                "X-Forwarded-For", "HTTP_CLIENT_IP", "HTTP_X_FORWARDED_FOR",
+                "HTTP_X_FORWARDED", "HTTP_FORWARDED_FOR", "HTTP_FORWARDED",
+                "Proxy-Client-IP", "WL-Proxy-Client-IP", "HTTP_VIA", "IPV6_ADR"
+        );
+
+        for (String header : headerList) {
+            clientIp = request.getHeader(header);
+            if (StringUtils.hasText(clientIp) && !clientIp.equalsIgnoreCase("unknown")) {
+                userLotto.setCrtIp(clientIp);
+                break;
+            }
+        }
+
+        if (!StringUtils.hasText(clientIp)) {
+            // 그렇지 않은 경우에는 request.getRemoteAddr()를 통해 기본적인 IP 주소를 얻기
             userLotto.setCrtIp(request.getRemoteAddr());
         }
+
         return body;
     }
 }
--- src/main/java/com/lotto/store/repository/mapper/StoreMapper.java
+++ src/main/java/com/lotto/store/repository/mapper/StoreMapper.java
@@ -4,7 +4,6 @@ import com.lotto.store.dto.Store;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Map;
 
 @Mapper
 public interface StoreMapper {
